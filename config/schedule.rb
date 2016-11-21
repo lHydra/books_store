9316cbdb319ff4d6fc2fbf67b03cff5368bfdd1c
George Melnikov <skywalqqer@yandex.ru>
2016-11-22 01:59:53 +0400

    fix schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,5 +2,5 @@ env :PATH, ENV['PATH']
 set :output, path + '/log/cron.log'
 
 every 3.minute do
-  runner "Coupon.where(created_at: (Time.now-5.minute)..Time.now).map(&:delete)"
+  runner "Coupon.where('created_at < ?', 5.minute.ago).map(&:delete)"
 end
